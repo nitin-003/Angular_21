ce077540dba71f7b7aebc3fc67cb7f0993f57297
Nitin Pathak <nitinpathak8242@gmail.com>
2026-01-17 13:25:59 +0530

    Two way data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,34 @@
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserData } from './user-data/user-data';
-import { AdminData } from './admin-data/admin-data';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,              
-  imports: [RouterOutlet, UserData, AdminData],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']        
 })
 
 export class App {
-  status = signal("notStarted")
+  userData = signal({
+    name: "Nitin Pathak",
+    age: 23,
+    email: "nitin@gmail.com"
+  })
 
-  handleSwitch(event:Event){
-    const target = event.target as HTMLSelectElement
-    this.status.set(target.value);
+  updateUserData(key:string, val:string){
+    this.userData.update((item) => ({...item, [key]: val}));
+    // if(key == 'name'){
+    //   this.userData.update((item) => ({...item, name: val}));
+    // }
+    // if(key == 'age'){
+    //   this.userData.update((item) => ({...item, age: parseInt(val)}));
+    // }
+    // if(key == 'email'){
+    //   this.userData.update((item) => ({...item, email: val}));
+    // }
   }
 }
+
 
