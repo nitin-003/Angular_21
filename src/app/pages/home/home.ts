82fa93aef44352472ee3e494487bce2b07f44a02
Nitin Pathak <nitinpathak8242@gmail.com>
2026-01-18 17:20:22 +0530

    Page and Redirection and Wildcard Route

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
+  constructor(public router:Router){}
 
+  goToProfilePage(){
+    this.router.navigate(['profile'])
+  }
 }
+
