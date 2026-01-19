9a6dbe6b6f257d27d836d39a4f3d5c9de20d48b0
Nitin Pathak <nitinpathak8242@gmail.com>
2026-01-19 17:39:19 +0530

    Template Driven Form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,35 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
-import { email, form, FormField, maxLength, minLength, required } from '@angular/forms/signals';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormField],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  styleUrls: ['./app.css'],
 })
 
-export class App{
-  loginModel = signal({email:'', password:''})
-  loginForm = form(this.loginModel, (f) => {
-    required(f.email, {message: "Please enter email address"})
-    minLength(f.email, 5, {message: "Please enter a valid email"})
-    email(f.email, {message: "Please enter valid email address"})
-    required(f.password, {message: "Please enter password"})
-    minLength(f.password, 5, {message: "Please enter valid password"})
-    maxLength(f.password, 15, {message: "Please enter valid password"})
-  })
+export class App {
+  userDetails: any;
 
-  login(){
-    console.log(this.loginForm.email().value())
-    console.log(this.loginForm.password().value())
-  }
-
-  reset(){
-    this.loginForm.email().value.set('')
-    this.loginForm.password().value.set('')
+  addUser(form: NgForm) {
+    console.log(form.value);
+    this.userDetails = form.value;
+    form.reset();
   }
 }
 
