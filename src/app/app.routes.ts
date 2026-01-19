1be2435034ebaa4bb82fda5c4392b2a502395bc1
Nitin Pathak <nitinpathak8242@gmail.com>
2026-01-19 12:53:00 +0530

    Lazy Loading with Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { User } from './pages/user/user';
 import { Profile } from './pages/profile/profile';
 import { Setting } from './pages/setting/setting';
+import { Admin } from './pages/admin/admin';
 
 export const routes: Routes = [
     {path:'', component:Home},
-    {path:'user', component:User, 
+    {path:'admin', loadComponent: () => import('./pages/admin/admin').then((c) => c.Admin)},
+    {path:'user', 
+        loadComponent: () => import('./pages/user/user').then((c) => c.User), 
         children: [
             {path:'', redirectTo:'profile', pathMatch:'full'},
             {path:'profile', component:Profile},
@@ -14,4 +16,5 @@ export const routes: Routes = [
         ]
     }
 ];
+
 
